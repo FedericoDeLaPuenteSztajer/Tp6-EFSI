5ae6974d8977207357ed0a918ec6e315182bab98
Federico de la Puente Sztajer <49319314@est.ort.edu.ar>
2026-05-12 15:57:37 -0300

    I diseñando BigPost

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -1,27 +1,27 @@
 import SmallPost from './SmallPost.jsx'
 
-function Account(props){
+function Account(props) {
 
-    const accout= props.Account;
+    const accout = props.Account;
 
-    return(
+    return (
         <>
-        <div>
-            <h2>{accout.name}</h2>
-            <img src={account.img}/>
-            <p>{accout.desc}</p>
+            <div>
+                <h2>{accout.name}</h2>
+                <img src={account.img} />
+                <p>{accout.desc}</p>
 
-            <ul>
-                <li>Cantidad de Publicaciones: {accout.cantPosts}</li>
-                <li>Cantidad de Seguidores: {accout.cantFollowers}</li>
-                <li>Cantidad de Seguidos: {accout.cantFollows}</li>
-            </ul>
+                <ul>
+                    <li>Cantidad de Publicaciones: {accout.cantPosts}</li>
+                    <li>Cantidad de Seguidores: {accout.cantFollowers}</li>
+                    <li>Cantidad de Seguidos: {accout.cantFollows}</li>
+                </ul>
 
-            {/** Simulador de posts */}
-            <SmallPost postImg={props.postImg} accountImg={account.img} accountName={accout.name}/>
+                {/** Simulador de posts */}
+                <SmallPost postImg={props.postImg} accountImg={account.img} accountName={accout.name} />
 
-            {props.actualAccount && <button>Editar</button>}
-        </div>
+                {props.actualAccount && <button>Editar</button>}
+            </div>
         </>
     )
 }
@@ -30,13 +30,14 @@ export default Account;
 
 /**
 Requisitos:
-
 - Nombre de usuario.
 - Foto de perfil.
 - Biografía breve.
+
 - Cantidad de publicaciones.
 - Cantidad de seguidores.
 - Cantidad de seguidos.
+
 - Publicaciones asociadas al perfil.
 - Botón visual de editar perfil o configuración.
  */
--- src/Components/BigPost.jsx
+++ src/Components/BigPost.jsx
@@ -1,12 +1,38 @@
 //
 
-function BigPost(){
+function BigPost(props) {
 
-    return(
+    const postData = props.postData
+
+    return (
         <>
-        
+            <img src={postData.postImg} />
+            <h2>{postData.accountName}</h2>
+            <p>{postData.desc}</p>
+
+            <ul>
+                <li>Likes: {postData.likes}</li>
+                <li>Fecha: {postData.date}</li>
+                <button>Comentar</button>
+            </ul>
+
+            {/** Comentarios: ¿Cómo hacerlos? */}
+
         </>
     )
 }
 
 export default BigPost;
+
+/** 
+Requisitos:
+- Imagen ampliada.
+- Nombre de usuario.
+
+- Descripción o caption.
+- Cantidad de likes.
+- Botones de interacción.
+- Fecha o información adicional del posteo.
+
+- Comentarios simulados.
+*/
