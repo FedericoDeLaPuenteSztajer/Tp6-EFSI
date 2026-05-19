04b3f11dcf7ddc3cf2c20d23f452474932be20c9
Federico de la Puente Sztajer <49319314@est.ort.edu.ar>
2026-05-19 14:07:51 -0300

    feat(account): pequeño ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ import Header from './Components/Header.jsx'
 import Account from './Components/Account.jsx'
 import Feed from './Components/Feed.jsx'
 import catApi from './Modules/TheCatApi.jsx'
+//Random entre 0 y 10 => Math.floor(Math.random() * 11)
 
 function App() {
 
+  const [actualAccount, setActualAccount] = useEffect({})
+  
   //Harcodeado
   const comments = ["Nuevo seguidor", "Yo literal", "primer comentario", "Le paso a mi tio", "Sófocles tenía razón", "Revivan la grasa :v"];
   const accountNames = ["AinzOoalGown", "Albedo", "ShalltearBlodfallen", "MareBelloFiore", "AuraBelloFiore", "Cockytus", "Demiurge", "PandorasActor"]
-
-  //Random entre 0 y 10 => Math.floor(Math.random() * 11)
-
-  const [actualAccount, setActualAccount] = useEffect({})
   let userAccount = {}
 
   userAccount["name"] = Math.floor(Math.random() * (accountNames.length + 1))
@@ -21,12 +20,15 @@ function App() {
   userAccount["cantPosts"] = Math.floor(Math.random() * (1, 251))
   userAccount["cantFollowers"] = Math.floor(Math.random() * (1, 1501))
   userAccount["cantFollows"] = Math.floor(Math.random() * (1, 21))
-
   catApi.get("?limit=2")
     .then((response) => {
       userAccount["img"] = response.data[0].url
       userAccount["firstPostImg"] = response.data[1].url
     })
+
+  setActualAccount(userAccount);
+
+  
 
   /** 
   const Login = () => {
